fafeb3af033741dfbfb03b4aeb24e04bd9c46d31
Ulises 4061 <223220@ids.upchiapas.edu.mx>
2026-03-12 16:57:33 -0600

    Fix language statistics: Remove generated Java build files from Git tracking and update .gitignore

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_gastosapp_GastosApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.gastosapp.GastosApplication"
-)
-public final class _com_gastosapp_GastosApplication {
-}
